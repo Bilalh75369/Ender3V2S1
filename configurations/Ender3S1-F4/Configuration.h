6cb8ac803948d8d47d9fc8471d2348e0f2dd64a7
Bilalh75369 <aliraza75369@gmail.com>
2024-06-15 17:19:05 +0500

    Update Configuration.h

--- configurations/Ender3S1-F4/Configuration.h
+++ configurations/Ender3S1-F4/Configuration.h
@@ -1815,15 +1815,15 @@
 // @section geometry
 
 // The size of the printable area
-#define X_BED_SIZE 220  // MRiscoC Max usable bed size
-#define Y_BED_SIZE 220  // MRiscoC Max usable bed size
+#define X_BED_SIZE 230  // MRiscoC Max usable bed size
+#define Y_BED_SIZE 270  // MRiscoC Max usable bed size
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS -10  // MRiscoC Stock physical limit
 #define Y_MIN_POS -8  // MRiscoC Stock physical limit
 #define Z_MIN_POS 0
 #define X_MAX_POS 230  // MRiscoC Stock physical limit
-#define Y_MAX_POS 220  // MRiscoC Stock physical limit
+#define Y_MAX_POS 270  // MRiscoC Stock physical limit
 #define Z_MAX_POS 270  // Ender Configs
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
